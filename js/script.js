f4eebdd604fd4eb769e610b366082ca90978b994
phat4life <phat4lifetv@gmail.com>
2025-08-05 07:51:20 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+let angle = 0;
+const carousel = document.querySelector('.carousel-container');
+const prev    = document.getElementById('prev');
+const next    = document.getElementById('next');
+
+prev.addEventListener('click', () => {
+  angle -= 120;
+  carousel.style.transform = `rotateY(${angle}deg)`;
+});
+
+next.addEventListener('click', () => {
+  angle += 120;
+  carousel.style.transform = `rotateY(${angle}deg)`;
+});
 
